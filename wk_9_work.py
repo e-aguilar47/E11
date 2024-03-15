a60c3317e15fa02680a0171fa79b7799167c042c
Bizzle653 <158625194+Bizzle653@users.noreply.github.com>
2024-03-15 16:27:12 -0700

    Add files via upload
    Radiation Detecting Code

---
+++ wk_9_work.py
@@ -0,0 +1,16 @@
+import RPi.GPIO as GPIO
+import time
+
+counts=0
+
+def my_callback(channel):
+   ptint(f"Count detected at{time.time()}")
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(26, GPIO.IN)
+GPIO.add_event_detect(17, GPIO.FALLING, callback=my_callback)
+
+while True:
+   time.sleep(20)
+   print(f"Number of counts measured is {counts}")
+ 
