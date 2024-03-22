32bde15deb39818c4a0bc7e7b1dfb5a32782db64
e-aguilar47 <158625168+e-aguilar47@users.noreply.github.com>
2024-03-22 15:12:49 -0700

    Update wk_9_work.py

--- wk_9_work.py
+++ wk_9_work.py
@@ -19,8 +19,7 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(26, GPIO.IN)
 GPIO.add_event_detect(26, GPIO.FALLING, callback=my_callback)
 
-now=time.time()
-while (now-start_time)<run_time:
+while (time.time()-start_time)<run_time:
    time.sleep(10)
    print(f"Number of counts measured is {counts}")
  
