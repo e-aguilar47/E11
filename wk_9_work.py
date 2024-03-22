5cf8ba0ee54fd838fcdc66d1448c4799a408f702
e-aguilar47 <158625168+e-aguilar47@users.noreply.github.com>
2024-03-22 14:50:38 -0700

    Update wk_9_work.py

--- wk_9_work.py
+++ wk_9_work.py
@@ -2,6 +2,8 @@ import RPi.GPIO as GPIO
 import time
 
 counts=0
+start_time=time.time
+run_time=int(sys.argv[1])
 
 def my_callback(channel):
    print(f"Count detected at{time.time()}")
@@ -10,7 +12,8 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(26, GPIO.IN)
 GPIO.add_event_detect(26, GPIO.FALLING, callback=my_callback)
 
-while True:
+now=time.time
+while (now-start_time)<run_time:
    time.sleep(10)
    print(f"Number of counts measured is {counts}")
  
