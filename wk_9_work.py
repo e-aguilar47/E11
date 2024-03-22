8c9a230d963a6a162ed51bee108e341fc1339fdd
e-aguilar47 <158625168+e-aguilar47@users.noreply.github.com>
2024-03-22 14:29:24 -0700

    Update wk_9_work.py

--- wk_9_work.py
+++ wk_9_work.py
@@ -11,6 +11,6 @@ GPIO.setup(26, GPIO.IN)
 GPIO.add_event_detect(26, GPIO.FALLING, callback=my_callback)
 
 while True:
-   time.sleep(20)
+   time.sleep(10)
    print(f"Number of counts measured is {counts}")
  
