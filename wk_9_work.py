08b24cc470f8a4af14607e0b17d47369730df81a
e-aguilar47 <158625168+e-aguilar47@users.noreply.github.com>
2024-03-22 15:23:05 -0700

    Update wk_9_work.py

--- wk_9_work.py
+++ wk_9_work.py
@@ -7,6 +7,7 @@ counts=0
 start_time=time.time()
 run_time=int(sys.argv[1])
 
+meta_data=['time', 'counts']
 filename=sys.argv[2]
 file=open(filename,'w',newline='')
 writer=csv.writer(file)
@@ -15,7 +16,7 @@ writer.writerow(meta_data)
 def my_callback(channel):
    global counts
    counts+=1
-   print(f"Count detected at{time.time()}")
+   print(f"Count detected at {time.time()}")
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(26, GPIO.IN)
@@ -23,5 +24,6 @@ GPIO.add_event_detect(26, GPIO.FALLING, callback=my_callback)
 
 while (time.time()-start_time)<run_time:
    time.sleep(10)
+   global counts=0
    print(f"Number of counts measured is {counts}")
  
