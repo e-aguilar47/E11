7a66a69d9521557263ab925cc970d87f6ccdd105
e-aguilar47 <158625168+e-aguilar47@users.noreply.github.com>
2024-03-22 14:57:58 -0700

    Update wk_9_work.py

--- wk_9_work.py
+++ wk_9_work.py
@@ -4,7 +4,7 @@ import sys
 import csv
 
 counts=0
-start_time=time.time
+start_time=time.time()
 run_time=int(sys.argv[1])
 
 filename='data.csv'
@@ -19,7 +19,7 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(26, GPIO.IN)
 GPIO.add_event_detect(26, GPIO.FALLING, callback=my_callback)
 
-now=time.time
+now=time.time()
 while (now-start_time)<run_time:
    time.sleep(10)
    print(f"Number of counts measured is {counts}")
